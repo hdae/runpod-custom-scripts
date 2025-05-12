9e4805a549c02e9a3d618d1fcbb3ec840bc6cf83
Hedudae <98353469+hdae@users.noreply.github.com>
2025-05-13 07:53:50 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Custom Script for hdae's RunPod template.
+
+RunPodのテンプレートに含むべきではない個人設定などを管理するリポジトリ。  
+カスタムスクリプトのサンプルも兼ねている。
---
+++ stable-diffusion-forge/custom_script.sh
@@ -0,0 +1,11 @@
+# Add extension "a1111-sd-webui-tagcomplete"
+git clone --depth=1 "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git" /workspace/extensions/tag-autocomplete
+
+# Install danbooru-tagcomplete-extra (Japanese tag completion)
+rm /workspace/extensions/tag-autocomplete/tags/danbooru.csv
+curl -fsS "https://github.com/hdae/danbooru-tagcomplete-extra/releases/download/2025-05-06/danbooru.csv" -o /workspace/extensions/tag-autocomplete/tags/danbooru.csv
+
+# Add config files.
+SDF_CONFIG="https://raw.githubusercontent.com/hdae/runpod-custom-scripts/refs/heads/main/stable-diffusion-forge"
+curl -fsS "${SDF_CONFIG}/config.json" -o /workspace/config.json
+curl -fsS "${SDF_CONFIG}/ui-config.json" -o /workspace/ui-config.json
