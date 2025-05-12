86456272d69305a652507ca606a3760f9ae61db9
Hedudae <98353469+hdae@users.noreply.github.com>
2025-05-13 08:07:42 +0900

    fix: use -fsSL

--- stable-diffusion-forge/custom_script.sh
+++ stable-diffusion-forge/custom_script.sh
@@ -3,9 +3,9 @@ git clone --depth=1 "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.g
 
 # Install danbooru-tagcomplete-extra (Japanese tag completion)
 rm /workspace/extensions/tag-autocomplete/tags/danbooru.csv
-curl -fsS "https://github.com/hdae/danbooru-tagcomplete-extra/releases/download/2025-05-06/danbooru.csv" -o /workspace/extensions/tag-autocomplete/tags/danbooru.csv
+curl -fsSL "https://github.com/hdae/danbooru-tagcomplete-extra/releases/download/2025-05-06/danbooru.csv" -o /workspace/extensions/tag-autocomplete/tags/danbooru.csv
 
 # Add config files.
 SDF_CONFIG="https://raw.githubusercontent.com/hdae/runpod-custom-scripts/refs/heads/main/stable-diffusion-forge"
-curl -fsS "${SDF_CONFIG}/config.json" -o /workspace/config.json
-curl -fsS "${SDF_CONFIG}/ui-config.json" -o /workspace/ui-config.json
+curl -fsSL "${SDF_CONFIG}/config.json" -o /workspace/config.json
+curl -fsSL "${SDF_CONFIG}/ui-config.json" -o /workspace/ui-config.json
