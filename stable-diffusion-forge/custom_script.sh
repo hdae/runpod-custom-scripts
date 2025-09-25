999e0f63aee703643ca644e6aaed2062c5784c5d
Hedudae <98353469+hdae@users.noreply.github.com>
2025-09-25 21:02:27 +0900

    update csv

--- stable-diffusion-forge/custom_script.sh
+++ stable-diffusion-forge/custom_script.sh
@@ -9,7 +9,7 @@
 
 # Install danbooru-tagcomplete-extra (Japanese tag completion)
 rm /workspace/extensions/tag-autocomplete/tags/danbooru.csv
-curl -fsSL "https://github.com/hdae/danbooru-tagcomplete-extra/releases/download/2025-05-06/danbooru.csv" -o /workspace/extensions/tag-autocomplete/tags/danbooru.csv
+curl -fsSL "https://github.com/hdae/danbooru-tagcomplete-extra/releases/download/2025-09-25/danbooru.csv" -o /workspace/extensions/tag-autocomplete/tags/danbooru.csv
 
 # # Add config files.
 # SDF_CONFIG="https://raw.githubusercontent.com/hdae/runpod-custom-scripts/refs/heads/main/stable-diffusion-forge"
